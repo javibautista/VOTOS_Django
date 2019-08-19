010b00fc5637a471f2bdec460c832cb7a73c5042
Javier Bautista <javibautista333@gmail.com>
2019-08-19 20:41:53 -0300

    borrando

--- polls/views.py
+++ polls/views.py
@@ -44,32 +44,4 @@ def results(request, question_id):
     print('OPCIONES %s'%(opciones))
     print('ELEGIDOS %s'%(elegidas))
 
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            #'error_message': "You didn't select a choice.",
-            'error_message': "No seleccionaste una opción.",
-        })
-    else:
-        selected_choice.respuesta
-        #selected_choice.votes += 1
-        #selected_choice.save()
-        c = {}
-        for opcion in opciones:
-            if respuesta:
-                c[opcion] = question.id in elegidos
-            else:
-                c[opcion] = question.id not in elegidos
-        # Always return an HttpResponseRedirect after successfully dealing
-        # Siempre devolver un HttpResponseRedirect después de tratar con éxito
-        # with POST data. This prevents data from being posted twice if a
-        # con datos POST. Esto evita que los datos se publiquen dos veces si
-        # user hits the Back button.
-        # usuario pulsa el botón Atrás.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-        print('selected_choice %s'%(selected_choice))
-    #return HttpResponse("You're voting on question %s." % question_id)
 
